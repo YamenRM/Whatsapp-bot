7e21b2e945ed8e6ed4541eee36936c080d15ce41
YamenRM <yamenrafat132@gmail.com>
2026-06-27 16:14:10 +0300

    Finshed Rag engine

---
+++ src/Rag_engine.py
@@ -0,0 +1,41 @@
+import os
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_chroma import Chroma 
+from src.config import settings
+
+def initialize_rag_system(pdf_path: str):
+
+    if not os.path.exists(pdf_path):
+        raise FileNotFoundError(f"ملف الـبيانات  غير موجود في المسار: {pdf_path}")
+        
+    print("⏳ ... جاري تحميل ملف الـبيانات وتقسيمه")
+    loader = PyPDFLoader(pdf_path)
+    documents = loader.load()
+    
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=settings.CHUNK_SIZE, 
+        chunk_overlap=settings.CHUNK_OVERLAP
+    )
+
+    chunks = text_splitter.split_documents(documents)
+    
+    print(f"✅ تم تقسيم الملف إلى {len(chunks)} جزء.")
+    
+
+    embeddings = GoogleGenerativeAIEmbeddings(model=settings.EMBEDDING_MODEL)
+    
+    print("💾 جاري توليد الـ Embeddings وحفظها في ChromaDB...")
+    vector_store = Chroma.from_documents(
+        documents=chunks,
+        embedding=embeddings,
+        persist_directory=settings.DB_DIR
+    )
+    print("🎯 تمت العملية بنجاح! قاعدة البيانات جاهزة للاستخدام.")
+    return vector_store
+
+def get_retriever():
+    embeddings = GoogleGenerativeAIEmbeddings(model=settings.EMBEDDING_MODEL)
+    vector_store = Chroma(persist_directory=settings.DB_DIR, embedding_function=embeddings)
+    return vector_store.as_retriever(search_kwargs={"k": 3}) # Top 3
